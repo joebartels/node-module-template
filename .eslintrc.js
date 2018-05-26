e9175e1ce7ac48357884725e5fa654cca0d06550
joseph bartels <joebartels@hotmail.com>
2018-05-26 17:17:42 -0400

    aphabetizing eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,23 @@
 
 module.exports = {
   root: true,
+
+  // https://eslint.org/docs/user-guide/configuring#specifying-parser
   parserOptions: {
-    ecmaVersion: 8,
-    sourceType: 'module'
+    ecmaVersion: 9,
+    sourceType: 'script',
+    ecmaFeatures: {
+      impliedStrict: true
+    }
   },
+
+
   extends: [
-    'eslint:recommended', 'google'
+    'eslint:recommended',
+    'google'
   ],
+
+  // https://eslint.org/docs/user-guide/configuring#specifying-environments
   env: {
     'browser': false,
     'node': true,
@@ -18,34 +28,56 @@ module.exports = {
     'rootRequire': 'off'
   },
   rules: {
+    // single line rules
     'arrow-parens': ['error', 'as-needed'],
-    'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }],
-    'space-before-function-paren': ['error', { 'anonymous': 'never', 'named': 'never' }],
+    'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
+    'comma-dangle': 'off',
+    'camelcase': 'error',
+    'max-statements-per-line': ['error', { 'max': 2 }],
+    'no-eval': 'error',
+    'no-multiple-empty-lines': ['error', { 'max': 1 }],
+    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_.+' }],
     'object-curly-spacing': ['error', 'always'],
+    'one-var': ['error', 'never'],
+    'operator-linebreak': ['error', 'after'],
     'prefer-const': 'error',
+    'semi-style': ['error', 'last'],
+
+    // multiline rules
     'indent': ['error', 2, {
-      'VariableDeclarator': {
-        'var': 2,
-        'let': 2,
-        'const': 3
-      },
       'SwitchCase': 1,
       'MemberExpression': 'off'
     }],
-    'no-multiple-empty-lines': ['error', { 'max': 1 }],
-    'no-unused-vars': ['error', { 'argsIgnorePattern': '^_.+' }],
-    'semi-style': ['error', 'last'],
-    'brace-style': ['error', '1tbs', { 'allowSingleLine': true }],
-    'max-statements-per-line': ['error', { 'max': 2 }],
-    'operator-linebreak': ['error', 'after'],
+
+    'max-len': ['error', {
+      'code': 80,
+      'comments': 100,
+      'ignoreTemplateLiterals': true,
+      'ignoreRegExpLiterals': true,
+    }],
+
     'new-cap': ['error', {
       'capIsNewExceptions': [
         'Fastify'
       ]
     }],
-    'comma-dangle': 'off',
-    'camelcase': 'error',
-    'no-eval': 'error',
-    'one-var': 'off'
-  }
+
+    'quotes': ['error', 'single', {
+      'avoidEscape': true,
+      'allowTemplateLiterals': true
+    }],
+
+    'space-before-function-paren': ['error', {
+      'anonymous': 'never',
+      'named': 'never'
+    }],
+  },
+  'overrides': [
+    {
+      'files': ['*-test.js'],
+      'rules': {
+        'max-len': 'off'
+      }
+    }
+  ]
 };
